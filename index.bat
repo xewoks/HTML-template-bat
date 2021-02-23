f50f328ed325775d8e56a8c6e63c6cc9d75dd134
xewoks <76702828+xewoks@users.noreply.github.com>
2021-02-23 10:00:53 -0500

    Add files via upload

---
+++ index.bat
@@ -0,0 +1,15 @@
+md css
+md js
+md img
+echo  ^<!doctype html^> >> index.html
+echo  ^<html lang="en"^> >> index.html
+echo  ^<head^> >> index.html
+echo  ^<meta charset="UTF-8"^> >> index.html
+echo  ^<title^> >> index.html
+echo  ^</title^> >> index.html
+echo  ^</head^> >> index.html
+echo  ^<body^> >> index.html
+echo  ^</body^> >> index.html
+echo  ^</html^> >> index.html
+echo  >>css\style.css
+pause
