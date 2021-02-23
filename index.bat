bac7c25f0ce3ea5bee89adcffccdb08fee7b3bbe
xewoks <76702828+xewoks@users.noreply.github.com>
2021-02-23 10:32:18 -0500

    Add files via upload
    Update 1.1 new languages enjoy

--- index.bat
+++ index.bat
@@ -1,3 +1,21 @@
+@ECHO off
+title Language templates
+cls
+:start
+ECHO.
+ECHO 1. HTML-template
+ECHO 2. Rust
+ECHO 3. Cpp
+set choice=
+set /p choice=Type the number to print text.
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto html
+if '%choice%'=='2' goto rust
+if '%choice%'=='3' goto Cpp
+ECHO "%choice%" is not valid, try again
+ECHO.
+goto start
+:html
 md css
 md js
 md img
@@ -12,4 +30,18 @@ echo  ^<body^> >> index.html
 echo  ^</body^> >> index.html
 echo  ^</html^> >> index.html
 echo  >>css\style.css
+echo  >>js\main.js
+goto end
+:rust
+md src
+echo  >> src\main.rs
+echo  >> cargo.toml
+echo  fn main() >>src\main.rs
+echo  println!("Hello, world!"); >>src\main.rs
+
+goto end
+:Cpp
+echo  >> main.cpp
+goto end
+:end
 pause
